5dfe00eff576b78c9a2d7028bb2ccf44657fc53a
andreaperlato <35103733+andreaperlato@users.noreply.github.com>
2018-09-13 11:52:29 +0200

    Update CASE statement.sql

--- CASE statement.sql
+++ CASE statement.sql
@@ -7,4 +7,25 @@ ELSE 'High'
 END
 FROM sales;
 
--- ihi
+-- Alias name to column
+SELECT SALES_DATE AS 'Sales Date', TOTAL_AMOUNT AS 'Total Amount',
+CASE
+WHEN TOTAL_AMOUNT <= 100 THEN 'Low'
+WHEN TOTAL_AMOUNT < 1000 THEN 'Medium'
+ELSE 'High'
+END AS 'Total Bracket'
+FROM sales;
+
+-- Select DISTINCT rows from the table: give the unique value
+SELECT DISTINCT ORDER_ID FROM sales -- don't repeat ORDER_ID numbers, nut jus give me unique ORDER_ID from sales table
+
+-- DISTINCT based on multiples columns
+SELECT DISTINCT OSALES_DATA, ORDER_ID FROM sales
+
+-- PSEUDO Columns
+SELECT SYSDATE, USER FROM DUAL
+SELECT ROWNUM, SALES_DATE, ORDER_ID FROM sales
+
+
+
+
